33e2e8697a3ac580e423728df8cd0eeb2277fa1d
bleach86 <14421739+bleach86@users.noreply.github.com>
2024-01-16 18:09:49 -0600

    remove external deps

--- python/cuatrorpc/cuatrorpc_rs.pyi
+++ python/cuatrorpc/cuatrorpc_rs.pyi
@@ -1,11 +1,11 @@
-from typing import List
+from typing import List, Dict, Any
 
-def callrpc_rs(url: str, method: str, params: str) -> List[int]: ...
+def callrpc_rs(url: str, method: str, wallet:str, params: List[Any]) -> Dict[str, Any]: ...
 def callrpc_cli_rs(
     cli_bin: str,
     data_dir: str,
     daemon_conf: str,
     method: str,
     wallet: str,
-    call_args: str,
-) -> str: ...
+    call_args: List[Any],
+) -> Dict[str, Any]: ...
